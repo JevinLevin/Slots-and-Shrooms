b7695aaceba8f6415eadf261862f280fad879373
Jevin <75545931+JevinLevin@users.noreply.github.com>
2026-06-15 13:09:50 +0100

    added game manager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using PrimeTween;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    public static GameManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+
+        Initialise();
+    }
+    #endregion
+
+    private void Initialise()
+    {
+        PrimeTweenConfig.warnEndValueEqualsCurrent = false;
+        PrimeTweenConfig.warnTweenOnDisabledTarget = false;
+        PrimeTweenConfig.warnZeroDuration = false;
+        PrimeTweenConfig.SetTweensCapacity(400);
+    }
+}
