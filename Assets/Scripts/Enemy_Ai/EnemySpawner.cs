7d7c49052a8bdc5526dbdb14835e5a6c6ce8c226
Jevin <75545931+JevinLevin@users.noreply.github.com>
2026-06-18 17:41:28 +0100

    spawning rework

--- Assets/Scripts/Enemy_Ai/EnemySpawner.cs
+++ Assets/Scripts/Enemy_Ai/EnemySpawner.cs
@@ -2,11 +2,12 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private SpawnPointStruct[] enemySpawnPoints;
-    [SerializeField] private GameObject[] enemyPrefabs; 
+    [SerializeField] private GameObject[] enemyPrefabs;
+    [SerializeField] private Vector2 spawnRadius = new Vector2(10, 25);
     private GameObject player; 
 
     private void Awake()
@@ -16,26 +17,44 @@ public class EnemySpawner : MonoBehaviour
 
     public List<EnemyBaseClass> SpawnEnemy(int enemiesToSpawn)
     {
-        Array.Sort(enemySpawnPoints, (a, b) =>
-        {
-            float distanceA = Vector3.Distance(player.transform.position, a.transform.position);
-            float distanceB = Vector3.Distance(player.transform.position, b.transform.position);
-
-            return distanceA.CompareTo(distanceB);
-        });
+        
 
         List<EnemyBaseClass> newEnemies = new(); 
         int j = 0;
         for(int i = 0; i < enemiesToSpawn; i++)
         {
-            if(j >= enemySpawnPoints.Length) j = 0;
+            // Random nav mesh position in range
+            int failSafe = 20;
+            Vector3 randomPos = player.transform.position;
+            while (failSafe > 0)
+            {
+                failSafe--;
+                randomPos = GetRandomPoint(player.transform.position, spawnRadius.y);
+
+                if (Vector3.Distance(player.transform.position, randomPos) < spawnRadius.x)
+                    continue;
+            }
+
 
             int roll = UnityEngine.Random.Range(0, enemyPrefabs.Length);
-            GameObject newEnemy = Instantiate(enemyPrefabs[roll], enemySpawnPoints[j].transform.position, Quaternion.identity);
+            GameObject newEnemy = Instantiate(enemyPrefabs[roll], randomPos, Quaternion.identity);
             newEnemies.Add(newEnemy.GetComponent<EnemyBaseClass>());
             j++;
         }
 
         return newEnemies;
+    }
+
+    public Vector3 GetRandomPoint(Vector3 center, float maxDistance)
+    {
+        // Get Random Point inside Sphere which position is center, radius is maxDistance
+        Vector3 randomPos = UnityEngine.Random.insideUnitSphere * maxDistance + center;
+
+        NavMeshHit hit; // NavMesh Sampling Info Container
+
+        // from randomPos find a nearest point on NavMesh surface in range of maxDistance
+        NavMesh.SamplePosition(randomPos, out hit, maxDistance, NavMesh.AllAreas);
+
+        return hit.position;
     }
 }
