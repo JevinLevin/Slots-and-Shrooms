4d4bbfae6f57f1ada49461d43cea36d1739f9c8e
DylanThyGamer <piemaster5556@gmail.com>
2026-06-18 17:47:50 +0100

    passive take dmg

--- Assets/Scripts/Mushroom/Attributes/Passive/PassiveMushroomAttributeSO.cs
+++ Assets/Scripts/Mushroom/Attributes/Passive/PassiveMushroomAttributeSO.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public abstract class PassiveMushroomAttributeSO : MushroomAttributeSO
 {
+    [SerializeField] float timer;
+    private float time = 0;
+
+    public override void OnTick()
+    {
+        time += 0.1f;
+        if (time < timer) return;
+        PassiveAbillity(); 
+        time = 0;
+    }
+
     public abstract void PassiveAbillity();
 }
